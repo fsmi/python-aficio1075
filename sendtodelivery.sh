d92af7899235330ec0c4a35c209e39a8f9c073f5
Fabian Knittel <fk@fsi-pc4.fsmi.local>
2007-12-21 00:17:51 +0100

    unreleased, unfinished and untested cleanups (would have been scanrouter 0.0.3)

--- sendtodelivery.sh
+++ sendtodelivery.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
-if [ x$1 == "x" ]; then
-echo "Usage: $0 <eigene IP-Adresse>"
-exit 1
+PRINTER_HOST="fsi-pr2"
+PRINTER_PORT="80"
+
+TARGET_IP="$1"
+if [ "$TARGET_IP" == "" ]; then
+	echo "Usage: $0 <eigene IP-Adresse>"
+	exit 1
 fi
 
-# Melde dich am Gerät an. Du bekommst dafür ein ticket, welches du im nächsten Aufruf brauchst.
+# Melde dich am Gerät an. Du bekommst dafür ein ticket, welches du im nächsten
+# Aufruf brauchst.
 locked=0
 while [ $locked -eq 0 ]; do
-info=$(cat delivery/auth-delivery.xml|netcat fsi-pr2 80|tail -n2)
-if [ $(echo $info|xmlstarlet sel -t -m '//*' -v 'returnValue') == "DIRC_OK" ]; then
-ticket=$(echo $info|xmlstarlet sel -t -m '//*' -v 'ticket_out/string')
-locked=1
-echo
-else
-sleep 1
-echo -n .
-fi
+	info=$(cat delivery/auth-delivery.xml | \
+	       netcat $PRINTER_HOST $PRINTER_PORT | \
+	       tail -n2)
+
+	return_value=$(echo $info | \
+	               xmlstarlet sel -t -m '//*' -v 'returnValue')
+	if [ "$return_value" == "DIRC_OK" ]; then
+		ticket=$(echo $info | \
+		         xmlstarlet sel -t -m '//*' -v 'ticket_out/string')
+		locked=1
+		echo
+	else
+		sleep 1
+		echo -n .
+	fi
 done
 # Wir haben unser Ticket bekommen.
 
-# Das hier funktioniert auf jedenfall für 10.14.1.1, 10.14.1.10 und 10.14.1.20. Bei 10.14.1.244 hat es Probleme gemacht... müsste man vielleicht noch verbessern.
-adresse=$(echo $1|perl -M"MIME::Base64" -ne 'print encode_base64($_)'|sed -e s/".="/"=="/)
+adresse=$(echo $TARGET_IP | \
+          perl -M"MIME::Base64" -ne 'print encode_base64($_)')
 
-# So.. jetzt können wir dem Drucker sagen, dass er uns Fragen soll wo er Scans abladen kann.
-cat delivery/set-delivery.xml|sed -e s/"TICKETSTRING"/"$ticket"/ -e s/"ADRESSE"/"$adresse"/|netcat fsi-pr2 80
+# Jetzt können wir dem Drucker sagen, dass er uns Fragen soll wo er Scans
+# abladen kann.
+cat delivery/set-delivery.xml | \
+	sed -e s/"TICKETSTRING"/"$ticket"/ \
+	    -e s/"ADRESSE"/"$adresse"/ | \
+	netcat $PRINTER_HOST $PRINTER_PORT
