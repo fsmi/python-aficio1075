9381d859b914cd67018c0e629ffd84b7aed80a38
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2008-06-22 21:20:29 +0200

    AficioJobs.py: Adds license and copyright statement.

--- AficioJobs.py
+++ AficioJobs.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim:set ts=4 sw=4 noet:
+
+# AficioJobs.py -- Adapter for the HTTP / HTML interface to print job control of
+#   Ricoh Aficio 1075
+#
+# Copyright (C) 2008 Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA.
+
+# Depends on python2.5
+
 
 import urllib
 from StringIO import StringIO
