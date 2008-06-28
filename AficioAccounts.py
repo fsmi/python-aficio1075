85adc9f02c155be7209a6adf5a7800494e5872df
Fabian Knittel <fabian.knittel@fsmi.uni-karlsruhe.de>
2008-06-28 14:58:43 +0200

    AficioAccount.py: Add UserStatistics class. Add from_xml loading methods.

--- AficioAccounts.py
+++ AficioAccounts.py
@@ -36,11 +36,92 @@ from xml import xpath
 
 STRING_ENCODING = 'Windows-1252'
 
-def _get_text_node(path, node):
-	return xpath.Evaluate('string(%s)' % path, node)
+def _decode(str, encoding = STRING_ENCODING):
+	if encoding == 'none' or encoding is None or encoding == '':
+		return str
+	else:
+		return codecs.getdecoder(encoding)(b64decode(str))
+
+def _encode(str, encoding = STRING_ENCODING):
+	if encoding == 'none' or encoding is None or encoding == '':
+		return str
+	else:
+		return b64encode(codecs.getencoder(encoding)(str)[0])
+
+
+def _get_text_node(path, base_node):
+	return xpath.Evaluate('string(%s)' % path, base_node)
 
 class UserMaintException(RuntimeError):
 	pass
+
+class UserStatistics(object):
+	pass
+	def __init__(self, copy_a4 = 0, copy_a3 = 0, print_a4 = 0,
+			print_a3 = 0, scan_a4 = 0, scan_a3 = 0):
+		self.copy_a4 = copy_a4
+		self.copy_a3 = copy_a3
+		self.print_a4 = print_a4
+		self.print_a3 = print_a3
+		self.scan_a4 = scan_a4
+		self.scan_a3 = scan_a3
+
+	def __str__(self):
+		return '<UserStatistics c%u,%u p%u,%u s%u,%u>' % (self.copy_a4,
+				self.copy_a3, self.print_a4, self.print_a3,
+				self.scan_a4, self.scan_a3)
+
+	def get_copy_a4_total(self):
+		return self.copy_a4 + (self.copy_a3 * 2)
+
+	def get_print_a4_total(self):
+		return self.print_a4 + (self.print_a3 * 2)
+
+	def get_scan_a4_total(self):
+		return self.scan_a4 + (self.scan_a3 * 2)
+
+	def to_xml(self):
+		return """
+			<statisticsInfo>
+				<copyInfo>
+					<monochrome>
+						<singleSize>%u</singleSize>
+						<doubleSize>%u</doubleSize>
+					</monochrome>
+				</copyInfo>
+				<printerInfo>
+					<monochrome>
+						<singleSize>%u</singleSize>
+						<doubleSize>%u</doubleSize>
+					</monochrome>
+				</printerInfo>
+				<scannerInfo>
+					<monochrome>
+						<singleSize>%u</singleSize>
+						<doubleSize>%u</doubleSize>
+					</monochrome>
+				</scannerInfo>
+			</statisticsInfo>""" % (self.copy_a4,
+				self.copy_a3, self.print_a4, self.print_a3,
+				self.scan_a4, self.scan_a3)
+
+	@staticmethod
+	def from_xml(stats_node):
+		assert stats_node.tagName == 'statisticsInfo'
+		return UserStatistics(
+				copy_a4 = int(_get_text_node(
+					'copyInfo/monochrome/singleSize', stats_node)),
+				copy_a3 = int(_get_text_node(
+					'copyInfo/monochrome/doubleSize', stats_node)),
+				print_a4 = int(_get_text_node(
+					'printerInfo/monochrome/singleSize', stats_node)),
+				print_a3 = int(_get_text_node(
+					'printerInfo/monochrome/doubleSize', stats_node)),
+				scan_a4 = int(_get_text_node(
+					'scannerInfo/monochrome/singleSize', stats_node)),
+				scan_a3 = int(_get_text_node(
+					'scannerInfo/monochrome/doubleSize', stats_node)))
+
 
 class UserRestrict(object):
 	def __init__(self, grant_copy = False, grant_printer = False,
@@ -51,8 +132,8 @@ class UserRestrict(object):
 		self.grant_storage = grant_storage
 
 	def __str__(self):
-		return '(c%dp%ds%dst%d)' % (self.grant_copy, self.grant_printer,
-				self.grant_scanner, self.grant_storage)
+		return '<UserRestrict c%dp%ds%dst%d>' % (self.grant_copy,
+				self.grant_printer, self.grant_scanner, self.grant_storage)
 
 	def __avail_str(self, is_available):
 		if is_available:
@@ -60,7 +141,7 @@ class UserRestrict(object):
 		else:
 			return '<restricted/>'
 
-	def get_as_xml(self):
+	def to_xml(self):
 		return """
 			<restrictInfo>
 				<copyInfo>
@@ -80,26 +161,80 @@ class UserRestrict(object):
 				self.__avail_str(self.grant_scanner),
 				self.__avail_str(self.grant_storage))
 
+	@staticmethod
+	def from_xml(restrict_node):
+		assert restrict_node.tagName == 'restrictInfo'
+		return UserRestrict(
+				grant_copy = len(xpath.Evaluate(
+						'copyInfo/monochrome/available', restrict_node)) == 1,
+				grant_printer = len(xpath.Evaluate(
+						'printerInfo/monochrome/available',
+						restrict_node)) == 1,
+				grant_scanner = len(xpath.Evaluate(
+						'scannerInfo/scan/available', restrict_node)) == 1,
+				grant_storage = len(xpath.Evaluate(
+						'localStorageInfo/plot/available', restrict_node)) == 1)
+
 class User(object):
-	def __init__(self, user_code, name, restrict = None):
+	def __init__(self, user_code, name, restrict = None, stats = None):
 		self.user_code = user_code
 		self.name = name
 		self.restrict = restrict
+		self.stats = stats
 
 	def __str__(self):
-		return 'User "%s" (#%u, %s)' % (self.user_code, self.name,
-				str(self.restrict))
+		return '<User "%s" (#%u, %s, %s)>' % (self.user_code, self.name,
+				str(self.restrict), str(self.stats))
 
-	def get_as_xml(self):
-		encoded_name = b64encode(
-				codecs.getencoder(STRING_ENCODING)(self.name)[0])
+	def to_xml(self):
+		encoded_name = _encode(self.name, STRING_ENCODING)
+		if self.restrict is None:
+			restr_str = ''
+		else:
+			restr_str = self.restrict.to_xml()
+		if self.stats is None:
+			stat_str = ''
+		else:
+			stat_str = self.stats.to_xml()
 		return """<user version="1.1">
 					<userCode>%u</userCode>
 					<userType>general</userType>
 					<userCodeName enc="%s">%s</userCodeName>
 					%s
+					%s
 				</user>""" % (self.user_code, STRING_ENCODING, encoded_name,
-					self.restrict.get_as_xml())
+					restr_str, stat_str)
+
+	@staticmethod
+	def from_xml(user_node):
+		assert user_node.tagName == 'user' \
+				and user_node.getAttribute('version') == '1.1'
+
+		# Load data
+		user_code = _get_text_node('userCode', user_node)
+		if user_code == 'other':
+			user_code = 0
+			name = 'other'
+		else:
+			user_code = int(user_code)
+			code_name_node = user_node.getElementsByTagName('userCodeName')[0]
+			name = _decode(_get_text_node('.', code_name_node),
+					code_name_node.getAttribute('enc'))
+
+		# Load sub-data
+		restrict_nodes = user_node.getElementsByTagName('restrictInfo')
+		if len(restrict_nodes) == 0:
+			restrict = None
+		else:
+			restrict = UserRestrict.from_xml(restrict_nodes[0])
+		stats_nodes = user_node.getElementsByTagName('statisticsInfo')
+		if len(stats_nodes) == 0:
+			stats = None
+		else:
+			stats = UserStatistics.from_xml(stats_nodes[0])
+
+		return User(user_code = user_code, name = name, restrict = restrict,
+				stats = stats)
 
 class UserMaintSession(object):
 	"""
@@ -123,7 +258,6 @@ class UserMaintSession(object):
 		(result, content) = h.request(uri, "POST", body = body,
 				headers = headers)
 
-		print content
 		reader = Sax2.Reader()
 		doc = reader.fromString(content)
 		return doc
@@ -158,7 +292,7 @@ class UserMaintSession(object):
 					</target>
 					%s
 				</addUserRequest>
-		""" % (user_code, u.get_as_xml())
+		""" % (user_code, u.to_xml())
 		doc = self._perform_operation(body)
 
 		error_code = self._get_operation_result(doc, 'addUserResult')
@@ -202,25 +336,9 @@ class UserMaintSession(object):
 
 		list = []
 		# Iterate over the users.
-		for user in xpath.Evaluate(
+		for user_node in xpath.Evaluate(
 				'operationResult/getUserInfoResult/result/user', doc):
-			user_code = _get_text_node('userCode', user)
-			if user_code == 'other':
-				user_code = 0
-				name = 'other'
-			else:
-				user_code = int(user_code)
-				name = codecs.getdecoder('windows-1252')(
-					b64decode(_get_text_node('userCodeName', user))
-					)[0]
-			copy_a3_count = int(_get_text_node(
-				'statisticsInfo/copyInfo/monochrome/doubleSize', user))
-			copy_a4_count = int(_get_text_node(
-				'statisticsInfo/copyInfo/monochrome/singleSize', user))
-			print_a3_count = int(_get_text_node(
-				'statisticsInfo/printerInfo/monochrome/doubleSize', user))
-			print_a4_count = int(_get_text_node(
-				'statisticsInfo/printerInfo/monochrome/singleSize', user))
-			list.append((user_code, name, print_a3_count, print_a4_count,
-					copy_a3_count, copy_a4_count))
+			u = User.from_xml(user_node)
+			list.append((u.user_code, u.name, u.stats.print_a3,
+					u.stats.print_a4, u.stats.copy_a3, u.stats.copy_a4))
 		return list
